5bde1d2140eec17a73b1414459cbd0c8f1f510ff
Giovanne Alves <63433253+giovanne-rf@users.noreply.github.com>
2021-05-30 10:02:03 -0300

    Pasta exception e arquivos

---
+++ backend/professor_allocation/src/main/java/com/project/professorallocation/exception/EntityInstanceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.project.professor.allocation.exception;
+
+import javax.persistence.EntityNotFoundException;
+
+public class EntityInstanceNotFoundException extends EntityNotFoundException {
+
+    private static final String MESSAGE_BASE = "Instance not found to ID \"%s\" of entity \"%s\"";
+
+    public <T> EntityInstanceNotFoundException(Class<T> entityClass, Object entityId) {
+        super(String.format(MESSAGE_BASE, entityClass, entityId));
+    }
+}
